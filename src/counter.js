0bd49c5e3306281f8110aa888b1e109c9c66fb1f
Josias Souza <josiascripto7@gmail.com>
2025-07-21 17:50:56 -0300

    pastas modificadas

--- src/counter.js
+++ src/counter.js
@@ -1,9 +1,9 @@
 export function setupCounter(element) {
-  let counter = 0
-  const setCounter = (count) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
-  }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
+
+  const a =0
+  const b =9
+  return a+b//teste commit
+
+
+
 }
